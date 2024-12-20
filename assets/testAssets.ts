a702fea51593ad6875f18bd8145de246fdfe3b41
Mihai Lapuste <mihailapuste3@gmail.com>
2024-12-19 16:45:37 -0800

    basic video testing

---
+++ assets/testAssets.ts
@@ -0,0 +1,64 @@
+const testAssets = {
+  1: {
+    local: require('./test1.mp4'),
+    remote: {
+      uri: 'https://d2518709tqai8z.cloudfront.net/videos/6779ed2f-f3de-46d8-adef-ea2ce410aeb8.mp4',
+    },
+  },
+  2: {
+    local: require('./test2.mp4'),
+    remote: {
+      uri: 'https://d2518709tqai8z.cloudfront.net/videos/7fe81a4f-bf15-4454-8fd2-c6f1d6b6f438.mp4',
+    },
+  },
+  3: {
+    local: require('./test3.mp4'),
+    remote: {
+      uri: 'https://d2518709tqai8z.cloudfront.net/videos/a0f60422-9740-4fc9-8be7-6837fa13fb85.mp4',
+    },
+  },
+  4: {
+    local: require('./test4.mp4'),
+    remote: {
+      uri: 'https://d2518709tqai8z.cloudfront.net/videos/55ba0168-c1bc-45c7-b673-321804bdc034.mp4',
+    },
+  },
+  5: {
+    local: require('./test5.mp4'),
+    remote: {
+      uri: 'https://d2518709tqai8z.cloudfront.net/videos/52f4c439-3a48-467e-b48c-81aada315a54.mp4',
+    },
+  },
+  6: {
+    local: require('./test6.mp4'),
+    remote: {
+      uri: 'https://d2518709tqai8z.cloudfront.net/videos/b33fbb18-e6c8-4dc0-958a-b70c3f84b97e.mp4',
+    },
+  },
+  7: {
+    local: require('./test7.mp4'),
+    remote: {
+      uri: 'https://d2518709tqai8z.cloudfront.net/videos/b79bb6c1-0f41-4ed5-a184-254daf9846df.mp4',
+    },
+  },
+  8: {
+    local: require('./test8.mp4'),
+    remote: {
+      uri: 'https://d2518709tqai8z.cloudfront.net/videos/7a215ece-18e7-4b5b-97db-4ee703108b68.mp4',
+    },
+  },
+  9: {
+    local: require('./test9.mp4'),
+    remote: {
+      uri: 'https://d2518709tqai8z.cloudfront.net/videos/cb273c20-0fc4-4e19-beea-5aa4b5ec7a9d.mp4',
+    },
+  },
+  10: {
+    local: require('./test10.mp4'),
+    remote: {
+      uri: 'https://d2518709tqai8z.cloudfront.net/videos/e7d92756-d389-4c94-90f7-76324de81df5.mp4',
+    },
+  },
+};
+
+export default testAssets;
--- src/screens/HomeScreen/ExpoScreens/ExpoAVVideoScreen/index.tsx
+++ src/screens/HomeScreen/ExpoScreens/ExpoAVVideoScreen/index.tsx
@@ -1,13 +1,79 @@
-import React from 'react';
-import {View, Text, StyleSheet, SafeAreaView, ScrollView} from 'react-native';
+import React, {useRef, useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  Button,
+} from 'react-native';
+import {Video, ResizeMode} from 'expo-av';
+import testAssets from '../../../../../assets/testAssets';
+
+const VIDEO_COUNT = 3;
 
 const ExpoAVVideoScreen: React.FC = () => {
+  const videoRefs = useRef<(Video | null)[]>(Array(VIDEO_COUNT).fill(null));
+  const [sourceIndex, setSourceIndex] = useState<number>(1);
+  const [sourceOrigin, setSourceOrigin] = useState<'local' | 'remote'>('local');
+
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const togglePlay = () => {
+    setIsPlaying(!isPlaying);
+  };
+
+  const previousSource = () => {
+    // index has to be between 1 and 10
+    const currentSourceIndex = sourceIndex;
+
+    if (currentSourceIndex === 1) {
+      setSourceIndex(10);
+    } else {
+      setSourceIndex(currentSourceIndex - 1);
+    }
+  };
+
+  const nextSource = () => {
+    // index has to be between 1 and 10
+    const currentSourceIndex = sourceIndex;
+
+    if (currentSourceIndex === 10) {
+      setSourceIndex(1);
+    } else {
+      setSourceIndex(currentSourceIndex + 1);
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
         <View style={styles.header}>
           <Text style={styles.headerTitle}>Expo-AV (Video)</Text>
         </View>
+
+        <Button title="Prev" onPress={previousSource} />
+        <Button title="Toggle Play" onPress={togglePlay} />
+        <Button title="Next" onPress={nextSource} />
+
+        {Array.from({length: VIDEO_COUNT}).map((_, index) => {
+          return (
+            <Video
+              key={index}
+              ref={el => (videoRefs.current[index] = el)}
+              style={styles.video}
+              source={testAssets[sourceIndex][sourceOrigin]}
+              useNativeControls={false}
+              isMuted={true}
+              shouldPlay={isPlaying}
+              resizeMode={ResizeMode.CONTAIN}
+              isLooping
+              onError={error =>
+                console.log(`ERROR: Video player ${index + 1}`, error)
+              }
+            />
+          );
+        })}
       </ScrollView>
     </SafeAreaView>
   );
@@ -20,6 +86,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#f2f2f7',
   },
+  video: {
+    width: '100%',
+    height: 300,
+    backgroundColor: '#000',
+    marginBottom: 10,
+  },
   header: {
     paddingHorizontal: 20,
     paddingTop: 20,
@@ -31,73 +103,5 @@ const styles = StyleSheet.create({
     color: '#000',
     fontWeight: 'bold',
     marginBottom: 10,
-  },
-  searchBox: {
-    backgroundColor: '#e0e0e0',
-    borderRadius: 10,
-    paddingVertical: 8,
-    paddingHorizontal: 10,
-  },
-  searchPlaceholder: {
-    color: '#8e8e93',
-    fontSize: 16,
-  },
-  profileCard: {
-    backgroundColor: '#fff',
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: 20,
-    marginHorizontal: 10,
-    borderRadius: 10,
-    marginBottom: 15,
-    marginTop: 10,
-  },
-  profileImage: {
-    width: 50,
-    height: 50,
-    borderRadius: 25,
-    marginRight: 15,
-  },
-  profileTextContainer: {
-    flex: 1,
-  },
-  profileName: {
-    color: '#000',
-    fontSize: 16,
-    fontWeight: '600',
-  },
-  profileDetails: {
-    color: '#8e8e93',
-    fontSize: 14,
-    marginTop: 2,
-  },
-  section: {
-    backgroundColor: '#fff',
-    marginHorizontal: 10,
-    borderRadius: 10,
-    marginBottom: 15,
-    overflow: 'hidden',
-  },
-  itemContainer: {
-    backgroundColor: '#fff',
-    paddingVertical: 15,
-    paddingHorizontal: 20,
-  },
-  itemContent: {
-    flexDirection: 'column',
-  },
-  itemTitle: {
-    color: '#000',
-    fontSize: 16,
-  },
-  itemSubtitle: {
-    color: '#8e8e93',
-    fontSize: 14,
-    marginTop: 2,
-  },
-  separator: {
-    height: StyleSheet.hairlineWidth,
-    backgroundColor: '#c7c7cc',
-    marginLeft: 20,
   },
 });
